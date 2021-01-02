56bbbaf3bd754d259cf652f8765e46ef6014b90c
JiHyunKim <arajh1009@naver.com>
2021-01-02 16:51:41 +0900

    Remember User Name
    greeting.js

--- greeting.js
+++ greeting.js
@@ -3,12 +3,23 @@ const form = document.querySelector(".js-form"),
   greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
-  SHOWING_CN = "showing";
+  SHOWING_CN = "showing"; //현재이름(CurrentName) 보여주기
 
+//USER_LS local Storage
+function savename(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+//인풋에 들어온 text를 <h4>에 나타내게 하려고 함
+//로직 -> 인풋에 들어온 이름을 로컬저장소에 넣고, 현재 값을 text로 보여주기
+// 로컬에 저장하는 이유는 새로고침했을 때도 사용자 이름 남아있게(기억하게) 하기 위함.
+// 이름을 바꾸고 싶다면 개발자모드 Application에 들어가서 key,value삭제하고 다시 입력
 function handleSubmit(event) {
   event.preventDefault(); //폼의 기본동작 enter막기
-  const currentValue = input.value;
-  console.log(currentValue);
+  const currentValue = input.value; //인풋에 입력된 값을 현재 값 상수에 넣음
+  //console.log(currentValue); //잘 작동하는 지 확인
+  paintGreeting(currentValue);
+  savename(currentValue);
 }
 
 function askForName() {
