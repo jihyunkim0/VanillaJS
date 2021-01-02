b3c47084840cf6d30dbd976c3434538833d65eac
JiHyunKim <arajh1009@naver.com>
2021-01-03 02:11:08 +0900

    weather.js API
    날씨 API를 활용하여 온도, 장소 화면에 띄우기
    then -> 데이터 처리가 끝나길 기다리는 방법

--- weather.js
+++ weather.js
@@ -1,5 +1,30 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "91ca049913c76c402084e715a5fe8f8c";
 const COORDS = "coords";
+
+// 잘 동작하는 지 개발자 모드 Network에서 확인
+function getWeather(lat, lng) {
+  //데이터 가져오는 법 fetch() 사용
+  //then -> (이전 메소드가 완료된 다음 실행)데이터가 완전히 넘왔을 때 다음을 실행하라
+  //그니까 js에서 뭔가가 끝나길 기다리는 방법이 then
+  //JSON등 데이터 처리가 되지 않았는데 다음 걸 처리하면 안 됨.
+  //json을 가져오는 이유는 network데이터말고, body안에 있는 데이터를 보기 위해
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  ) //여기까지만 하면 pending(대기상태, 가져온 데이터 처리중)상태가 계속됨
+    .then(function (response) {
+      //console.log(response)
+      return response.json();
+    })
+    //응답이 끝나길 기다린 뒤 json데이터준비
+    .then(function (json) {
+      //console.log(json);
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 //로컬저장소에 위도 경도 저장(obj를 string으로 바꿈)
 function saveCoords(coordsOBJ) {
@@ -18,6 +43,7 @@ function handleGeoSucces(position) {
     longitude,
   };
   saveCoords(coordsOBJ);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -34,7 +60,11 @@ function loadCoords() {
     //위치정보 동의 하기 전
     askForCoords(); //좌표요청함수
   } else {
+    //위치정보를 가져오고 나서 실행
     //getWeather  //날씨요청함수
+    const parsedCoordes = JSON.parse(loadedCoords);
+    //console.log(parsedCoordes); //확인
+    getWeather(parsedCoordes.latitude, parsedCoordes.longitude);
   }
 }
 
